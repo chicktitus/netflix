e0e4eb09f5805184536a14fe2e093262b43b668c
Chick Titus <chicktitus24@gmail.com>
2021-01-07 01:11:36 -0800

    update getting the series and films from firebase

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import {useContent} from '../hooks/use-content';
 
 export default function Browse(){
-  
+  // we need the series and films
+  // we need slides
+  //pass it to the browse contaainer
   return <p>hello this is Browse page</p>
 };
