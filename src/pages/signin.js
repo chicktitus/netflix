0d25a90c191bf0048fa05002856568b5192fbf55
Chick Titus <chicktitus24@gmail.com>
2021-01-06 22:36:05 -0800

    update setting up the login form on the Signin page

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,5 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { FooterContainer } from '../containers/footer';
+import {HeaderContainer} from '../containers/header';
+import { Form } from '../components';
 
 export default function Signin(){
-  return <p>hello this is signin page</p>
+  const [emailAddress, setEmailAddress] = useState();
+  const [password, setPassword] = useState();
+  const [error, setError] = useState('');
+
+  const isInvalid = password === '' || emailAddress === '';
+  const handleSignIn = (event) => {
+    event.preventDefault();
+
+
+  }
+
+  return(
+  <>
+  <HeaderContainer>
+    <Form>
+     <Form.Title>Sign In</Form.Title>
+     {error && <Form.Error>{error}</Form.Error>}
+
+     <Form.Base onSubmit={handleSignIn} method='POST'>
+       <Form.Input
+        placeholder="Email Address"
+        value={emailAddress}
+        onChange={({ target})  => setEmailAddress (target.value)} />
+
+        <Form.Input
+        placeholder="Password"
+        type="password"
+        autoComplete="off"
+        value={password}
+        onChange={({ target})  => setPassword (target.value)} />
+
+          <Form.Submit disabled = {isInvalid} type="Submit">
+            Sign In
+          </Form.Submit>
+
+     </Form.Base>
+    </Form>
+  </HeaderContainer>
+  <FooterContainer />
+  </>
+  );
 };
