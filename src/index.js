f002a92b64600d3e70dbd453f85d1a1a72bf07b1
Chick Titus <chicktitus24@gmail.com>
2021-01-06 21:23:45 -0800

    seed data into firebase

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
 import 'normalize.css';
-import App from './app';  
+import App from './app';
 import {GlobalStyles} from './global-styles';
+import { firebase } from './lib/firebase.prod';
+import { FirebaseContext } from './context/firebase';
 
-render(<>
-<GlobalStyles /> 
+render(
+<>
+< FirebaseContext.Provider value={{ firebase }}>
+  <GlobalStyles />
 <App />
-</>,  
-document.getElementById('root'));
+</FirebaseContext.Provider>
+
+</>,
+document.getElementById('root')
+
+);
+
+
--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,12 +1,23 @@
 import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import { seedDatabase } from '../seed';
 
 // we need to seed the database
 
 //  config file here
-const config = {};
+const config = {
+   apiKey: "AIzaSyD-Y2Kp_w-tV9u44UFEgirGqzyrqiSedLw",
+    authDomain: "netflix-clone-92935.firebaseapp.com",
+    projectId: "netflix-clone-92935",
+    storageBucket: "netflix-clone-92935.appspot.com",
+    messagingSenderId: "622311001206",
+    appId: "1:622311001206:web:0e5475e9ebb49ad7546357"
+
+};
 
 const firebase = Firebase.initializeApp(config);
+
+//seedDatabase(firebase);
 
 export { firebase};
