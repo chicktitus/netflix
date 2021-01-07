c83b2d5c3d9684e3b3d6e312987edc5077ee4ac2
Chick Titus <chicktitus24@gmail.com>
2021-01-06 20:17:29 -0800

    update

---
+++ src/pages/browse.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Browse(){
+  return <p>hello this is Browse page</p>
+};
---
+++ src/pages/signin.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Signin(){
+  return <p>hello this is signin page</p>
+};
